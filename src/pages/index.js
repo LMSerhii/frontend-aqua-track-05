89c5c626044c4d00b7bf402c8db54708d6c9a063
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-05-08 11:22:31 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import { lazy } from 'react';
 
-export const HomePage = lazy(() => import('./HomePage'));
-export const SignInPage = lazy(() => import('./SignInPage'));
-export const SignUpPage = lazy(() => import('./SignUpPage'));
-export const TrackerPage = lazy(() => import('./TrackerPage'));
+export const HomePage = lazy(() => import('./HomePage.jsx'));
+export const SignInPage = lazy(() => import('./SigninPage.jsx'));
+export const SignUpPage = lazy(() => import('./SignupPage.jsx'));
+export const TrackerPage = lazy(() => import('./TrackerPage.jsx'));
 export const NotFoundPage = lazy(() => import('./NotFound.jsx'));
