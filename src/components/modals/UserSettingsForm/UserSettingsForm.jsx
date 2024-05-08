4c5e5c97b760d77bc9dc9aa1977fbf41448d59fb
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-05-08 19:09:13 +0300

    Update UserSettingsForm.jsx

--- src/components/modals/UserSettingsForm/UserSettingsForm.jsx
+++ src/components/modals/UserSettingsForm/UserSettingsForm.jsx
@@ -22,27 +22,40 @@ export const UserSettingsForm = () => {
 
   return (
     <>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <div>
-        <svg className={s.uploud} width="18" height="18">
-          <use xlinkHref={`${sprite}#upload`}></use>
-        </svg>
-        <p>Upload a photo</p>
-      </div>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="avatar">
+          <svg className={s.uploud} width="18" height="18">
+            <use xlinkHref={`${sprite}#upload`}></use>
+          </svg>
+          Upload a photo
+        </label>
+        <input
+          type="file"
+          id="avatar"
+          accept="image/*"
+          onChange={e => {
+            // Отримуємо файли, які обрано
+            const file = e.target.files[0];
+            // Якщо файл обрано, використовуємо URL.createObjectURL()
+            if (file) {
+              const imageUrl = URL.createObjectURL(file);
+              // Тут ви можете використати imageUrl для відображення аватарки
+              console.log('Temporary URL for the selected image:', imageUrl);
+            }
+          }}
+          {...register('avatar')}
+        />
+        <br />
         <label htmlFor="gender">Your gender identity</label>
         <br />
         <input
-          type="checkbox"
+          type="radio"
           id="female"
           value="female"
           {...register('gender')}
         />
-
         <label htmlFor="female">Female</label>
-        <input type="checkbox" id="male" value="male" {...register('gender')} />
+        <input type="radio" id="male" value="male" {...register('gender')} />
         <label htmlFor="male">Male</label>
         <br />
         <label htmlFor="Your_name">Your name</label>
@@ -53,6 +66,44 @@ export const UserSettingsForm = () => {
         <br />
         <input {...register('Email')} />
         <p>{errors.Email?.message}</p>
+        <div>
+          <h3>My daily norma</h3>
+          <p>For woman:</p>
+          <p>V=(M*0,03) + (T*0,4)</p>
+          <p>For man:</p>
+          <p>V=(M*0,04) + (T*0,6)</p>
+          <p>
+            * V is the volume of the water norm in liters per day, M is your
+            body weight, T is the time of active sports, or another type of
+            activity commensurate in terms of loads (in the absence of these,
+            you must set 0)
+          </p>
+          <div>
+            <svg className={s.uploud} width="18" height="18">
+              <use xlinkHref={`${sprite}#upload`}></use>
+            </svg>
+            <p>Active time in hours</p>
+          </div>
+        </div>
+        <label htmlFor="Your_weight">Your weight in kilograms:</label>
+        <br />
+        <input {...register('Your_weight')} />
+        <br />
+        <label htmlFor="Your_sports">
+          The time of active participation in sports:
+        </label>
+        <br />
+        <input {...register('Your_sports')} />
+        <br />
+        <p>The required amount of water in liters per day:</p>
+        <p>1.8 L</p>
+        <label htmlFor="Your_water">
+          Write down how much water you will drink:
+        </label>
+        <br />
+        <input {...register('Your_water')} />
+        <br />
+        <button type="submit">Save</button>
       </form>
     </>
   );
