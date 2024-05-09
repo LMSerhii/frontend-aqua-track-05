586bf3a4b944e6cd8ae10892a911d7f20b4522ba
RayChikS <vatoresp@gmail.com>
2024-05-09 15:30:14 +0300

    all-mobile-styles-added

--- src/components/modals/WaterForm/WaterForm.jsx
+++ src/components/modals/WaterForm/WaterForm.jsx
@@ -8,57 +8,111 @@ console.log(sprite);
 
 export const WaterForm = () => {
   const [count, setCount] = useState(50);
+  const [waterValue, setWaterValue] = useState('');
 
-  const onAddCount = () => {
-    setCount(count + 50);
+  const handleWaterChange = event => {
+    const value = event.target.value;
+    if (!isNaN(value)) {
+      setWaterValue(value);
+      setCount(parseInt(value, 10));
+    }
   };
+
+  const currentTime = new Date().toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+  const [time, setTime] = useState(currentTime);
+
+  const handleTimeChange = event => {
+    setTime(event.target.value);
+  };
+
+  // const onAddCount = () => {
+  //   setCount(count + 50);
+  // };
+  // const onRemoveCount = () => {
+  //   if (count === 0) {
+  //     setCount(0);
+  //   } else {
+  //     setCount(count - 50);
+  //   }
+  // };
+  const onAddCount = () => {
+    const newCount = count + 50;
+    setCount(newCount);
+    setWaterValue(newCount.toString());
+  };
+
   const onRemoveCount = () => {
-    if (count === 0) {
-      setCount(0);
-    } else {
-      setCount(count - 50);
+    if (count >= 50) {
+      const newCount = count - 50;
+      setCount(newCount);
+      setWaterValue(newCount.toString());
     }
   };
 
   return (
     <Modal className={s.modal} active={true}>
-      <svg className={s.icon} width="18" height="18">
-        <use xlinkHref={`${sprite}#close`}></use>
-      </svg>
+      <div className={s.content}>
+        <svg className={s.icon} width="28" height="28">
+          <use xlinkHref={`${sprite}#close`}></use>
+        </svg>
 
-      <h3 className={s.title}>Add water</h3>
+        <h3 className={s.title}>Add water</h3>
 
-      <p className={s.choose}>Choose a value:</p>
+        <p className={s.choose}>Choose a value:</p>
 
-      <p className={s.amount}>Amount of water:</p>
+        <p className={s.amount}>Amount of water:</p>
 
-      <div className={s.counter}>
-        <Button classname={s.btnCounter} onClick={onRemoveCount} type="button">
-          -
-        </Button>
-        <p className={s.countValue}>{count} ml</p>
-        <Button classname={s.btnCounter} onClick={onAddCount} type="button">
-          +
-        </Button>
+        <div className={s.counter}>
+          <Button
+            className={s.btnCounter}
+            onClick={onRemoveCount}
+            type="button"
+          >
+            -
+          </Button>
+          <p className={s.countValue}>{count} ml</p>
+          <Button className={s.btnCounter} onClick={onAddCount} type="button">
+            +
+          </Button>
+        </div>
+
+        <form className={s.form} action="">
+          <label className={s.timeLable} htmlFor="time">
+            Recording time:
+          </label>
+          <br />
+          <input
+            className={s.timeInput}
+            value={time}
+            onChange={handleTimeChange}
+            type="time"
+            id="time"
+            name="time"
+          />
+          <br />
+
+          <label className={s.waterLable} htmlFor="water">
+            Enter the value of the water used:
+          </label>
+          <br />
+          <input
+            className={s.waterInput}
+            value={waterValue}
+            onChange={handleWaterChange}
+            type="text"
+            name="water"
+            id="water"
+          />
+
+          <Button classname={s.btnSubmit} type="submit">
+            Save
+          </Button>
+        </form>
       </div>
-
-      <form className={s.form} action="">
-        <label className={s.timeLable} htmlFor="time">
-          Recording time:
-        </label>
-        <br />
-        <input className={s.timeInput} type="time" id="time" name="time" />
-        <br />
-        <label className={s.waterLable} htmlFor="water">
-          Enter the value of the water used:
-        </label>
-        <br />
-        <input className={s.waterInput} type="text" name="water" id="water" />
-
-        <Button classname={s.btnSubmit} type="submit">
-          Save
-        </Button>
-      </form>
     </Modal>
   );
 };
