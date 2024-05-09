4dec590205b1bfd78132f2603661db00ab7f582c
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-05-09 11:04:01 +0300

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,35 +1,13 @@
-// import { Helmet } from 'react-helmet-async';
-// import Home from '../components/homePage/Home';
-
-// export default function HomePage() {
-//   return (
-//     <>
-//       <Helmet>
-//         <title>Home Page</title>
-//       </Helmet>
-//       <Home />
-//     </>
-//   );
-// }
-
-import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Modal } from '../shared/components/Modal/Modal';
-import { UserSettingsModal } from '../components/modals/UserSettingsModal/UserSettingsModal.jsx';
+import Home from '../components/homePage/Home';
 
 export default function HomePage() {
-  const [isOpen, setIsOpen] = useState(true);
-
   return (
     <>
       <Helmet>
         <title>Home Page</title>
       </Helmet>
-      <p>Home page</p>
-      <button onClick={() => setIsOpen(true)}>Open modalSetting</button>
-      <Modal active={isOpen} setActive={setIsOpen}>
-        <UserSettingsModal />
-      </Modal>
+      <Home />
     </>
   );
 }
