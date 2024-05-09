16385ad7fe57776fe0be2b804519d7c2c1a6bbb4
Krystyna <zkristal95@gmail.com>
2024-05-09 16:31:38 -0400

    done 1-st part

--- src/components/trackerPage/WaterList/WaterList.jsx
+++ src/components/trackerPage/WaterList/WaterList.jsx
@@ -1,15 +1,16 @@
-import Button from '../../../shared/components/Button/Button';
 import { sprite } from '../../../shared/icons/index';
+import Button from '../../../shared/components/Button/Button';
 import ChooseDate from '../ChooseDate/ChooseDate';
+import WaterItem from '../WaterItem/WaterItem';
 import s from './WaterList.module.css';
 
 const WaterList = () => {
   return (
-    <>
-      <div className={s.wrap}>
+    <section className={s.waterListSection}>
+      <div className={s.waterListSectionHead}>
         <ChooseDate />
         <Button classname={s.addWaterBtn}>
-          <span className={s.wrapPlusIcon}>
+          <span className={s.plusIconWrap}>
             <svg className={s.plusIcon} width="20" height="20">
               <use xlinkHref={`${sprite}#plus`}></use>
             </svg>
@@ -17,7 +18,19 @@ const WaterList = () => {
           <span>Add water</span>
         </Button>
       </div>
-    </>
+
+      <ul className={s.waterListWrap}>
+        <WaterItem />
+        <WaterItem />
+        <WaterItem />
+        <WaterItem />
+        <WaterItem />
+        <WaterItem />
+        <WaterItem />
+        <WaterItem />
+        <WaterItem />
+      </ul>
+    </section>
   );
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import { Helmet } from 'react-helmet-async';
 import Home from '../components/homePage/Home';
-import WaterList from '../components/trackerPage/WaterList/WaterList';
 
 export default function HomePage() {
   return (
@@ -9,7 +8,6 @@ export default function HomePage() {
         <title>Home Page</title>
       </Helmet>
       <Home />
-      <WaterList />
     </>
   );
 }
