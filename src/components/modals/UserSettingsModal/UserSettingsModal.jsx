3dfbd0defd48df9837dd31a965f42a83c02fd6c2
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-05-09 10:34:39 +0300

    add modalWindow

--- src/components/modals/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/modals/UserSettingsModal/UserSettingsModal.jsx
@@ -22,3 +22,25 @@ export const UserSettingsModal = () => {
     </>
   );
 };
+
+// import { useState } from 'react';
+// import { Helmet } from 'react-helmet-async';
+// import { Modal } from '../shared/components/Modal/Modal';
+// import { UserSettingsModal } from '../components/modals/UserSettingsModal/UserSettingsModal.jsx';
+
+// export default function HomePage() {
+//   const [isOpen, setIsOpen] = useState(true);
+
+//   return (
+//     <>
+//       <Helmet>
+//         <title>Home Page</title>
+//       </Helmet>
+//       <p>Home page</p>
+//       <button onClick={() => setIsOpen(true)}>Open modalSetting</button>
+//       <Modal active={isOpen} setActive={setIsOpen}>
+//         <UserSettingsModal />
+//       </Modal>
+//     </>
+//   );
+// }
