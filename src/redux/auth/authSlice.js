2c97af7bb5872bb24d1ada213c7b929adafc8d0c
Pohrebniak Andrian <pogra29@gmail.com>
2024-05-10 22:49:45 +0300

    Update authSlice.js

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -18,6 +18,7 @@ const authSlice = createSlice({
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
+        state.isLoggedIn = true;
       })
       .addCase(logIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
