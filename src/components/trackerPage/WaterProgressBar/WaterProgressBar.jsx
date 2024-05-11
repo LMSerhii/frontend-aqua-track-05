3068bac9a5b15f862a15470e0c5f72cef475e975
blavikensbutcher <luckertheory@gmail.com>
2024-05-11 23:33:20 +0300

    remove margin top

--- src/components/trackerPage/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/trackerPage/WaterProgressBar/WaterProgressBar.jsx
@@ -4,14 +4,13 @@ import css from './WaterProgressBar.module.css'
 export const WaterProgressBar = () => {
   let windowWidth = window.innerWidth;
 
-  const pBarWidth = windowWidth < 767 ? "100%" : "100%"
   const pBarHeight = windowWidth < 767 ? 6 : 8
 
   return (
     <div>
       <p className={css.today_text}>Today</p>
     <Tooltip title="PERCENTAGE HERE TOO">
-      <Progress percent={10} size={[pBarWidth, pBarHeight]} showInfo={false} strokeColor="var(--lettuce)" />
+      <Progress percent={10} size={["100%", pBarHeight]} showInfo={false} strokeColor="var(--lettuce)" />
     </Tooltip>
       <div className={css.percent_wrap}>
       <span className={css.percent}>0%</span>
