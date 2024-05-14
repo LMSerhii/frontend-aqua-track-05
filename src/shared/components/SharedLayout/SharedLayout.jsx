e45866d9982683e78b8984d42fd2e42e46afdc80
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-05-15 00:23:02 +0200

    SignInForm

--- src/shared/components/SharedLayout/SharedLayout.jsx
+++ src/shared/components/SharedLayout/SharedLayout.jsx
@@ -2,12 +2,14 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import s from './SharedLayout.module.css';
+import Head from '../Head/Head';
 
 export default function SharedLayout() {
   return (
     <>
       <Suspense fallback={null}>
         <main className={s.container}>
+          <Head />
           <Outlet />
         </main>
       </Suspense>
