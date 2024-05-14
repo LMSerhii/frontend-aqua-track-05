9505a31fda4331ac3cd04bec7c34d42e78542ece
blavikensbutcher <luckertheory@gmail.com>
2024-05-14 20:54:21 +0300

    add modal on click add water

--- src/components/trackerPage/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/trackerPage/WaterMainInfo/WaterMainInfo.jsx
@@ -6,8 +6,13 @@ import bottle3x from '../../../shared/images/bottleImage/water_bottle@3x.png';
 import Logo from '../../../shared/components/Logo/Logo.jsx';
 import Button from '../../../shared/components/Button/Button.jsx';
 import SharedSVG from '../../../shared/components/SharedSVG/SharedSVG.jsx';
+import { WaterModal } from '../../modals/WaterModal/WaterModal.jsx';
+import { Modal } from '../../../shared/components/Modal/Modal.jsx';
+import { useState } from 'react';
 
 export const WaterMainInfo = () => {
+
+  const [activeAdd, setActiveAdd] = useState(false);
 
   const width = window.innerWidth;
 
@@ -48,7 +53,7 @@ export const WaterMainInfo = () => {
       </div>
 
       <div className={css.add_water_btn_wrap}>
-        <Button classname={css.add_water_btn} type="submit">
+        <Button classname={css.add_water_btn} type="submit" onClick={() =>  setActiveAdd(true)}>
           <SharedSVG  className={css.icon}
                       width={width < 768 ? "14" : "21"}
                       height={width < 768 ? "14" : "21"}
@@ -61,6 +66,15 @@ export const WaterMainInfo = () => {
       <div className={css.bottle_page_slider_wrapper}>
         <WaterProgressBar className={css.bottle_page_progress_bar} />
       </div>
+
+      <Modal active={activeAdd} setActive={setActiveAdd}>
+        <WaterModal
+          operation="add"
+          title="Add water"
+          subTitle="Choose a value"
+          setActive={setActiveAdd}
+        />
+      </Modal>
     </div>
   );
 };
