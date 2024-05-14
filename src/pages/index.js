9ae80a11a668d8b5472b487aae82ff37517feef6
cefariron <adidaswww17@gmail.com>
2024-05-14 18:06:48 +0300

    notify to verify and reset

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,3 +7,4 @@ export const TrackerPage = lazy(() => import('./TrackerPage.jsx'));
 export const NotFoundPage = lazy(() => import('./NotFound.jsx'));
 export const ForgotPasswordPage = lazy(() => import('./ForgotPasswordPage.jsx'));
 export const ResetPasswordPage = lazy(() => import('./ResetPasswordPage.jsx'));
+export const SuccessVerifyPage = lazy(() => import('./SuccessVerifyPage.jsx'));
