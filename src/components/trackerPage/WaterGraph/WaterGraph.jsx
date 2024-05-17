5d825e479ece151e0d3ee1bdf201c60d781b6d0b
Krystyna <zkristal95@gmail.com>
2024-05-17 12:15:15 -0400

    fixed styles statistics

--- src/components/trackerPage/WaterGraph/WaterGraph.jsx
+++ src/components/trackerPage/WaterGraph/WaterGraph.jsx
@@ -1,17 +1,14 @@
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  Tooltip, YAxis,
-} from 'recharts';
-
+import { AreaChart, Area, XAxis, Tooltip, YAxis } from 'recharts';
+import useMediaQuery from '../../../hooks/useMediaQuery';
 
 export const WaterGraph = () => {
+  const isDesktop = useMediaQuery('(min-width: 1440px)');
+  const isTablet = useMediaQuery('(min-width: 768px) and (max-width: 1439px)');
 
-  const innerWidth = window.innerWidth
-
-  const paddingGraph = innerWidth > 767 ? 40 : 20
-
+  const paddingGraph = isTablet ? 36 : 10;
+  const chartWidth = isDesktop ? 608 : isTablet ? 640 : 303;
+  const chartHeight = isDesktop || isTablet ? 269 : 257;
+  const wrapperPadding = isDesktop || isTablet ? '49px' : '35px';
 
   const data = [
     { date: '16', Water: 1.5 },
@@ -23,35 +20,59 @@ export const WaterGraph = () => {
     { date: '22', Water: 2 },
   ];
 
-
-
   return (
-    <AreaChart
-      width={608}
-      height={360}
-      data={data}
-      margin={{
-        top: 50,
-        right: 0,
-        left: 0,
-        bottom: 0,
-      }}
-    >
-      <defs>
-        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="40%" stopColor="var(--lettuce)" stopOpacity={1} />
-          <stop offset="100%" stopColor="var(--lettuce)" stopOpacity={0} />
-        </linearGradient>
-      </defs>
+    <div style={{ paddingTop: wrapperPadding }}>
+      <AreaChart width={chartWidth} height={chartHeight} data={data}>
+        <defs>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="40%" stopColor="var(--lettuce)" stopOpacity={1} />
+            <stop offset="100%" stopColor="var(--lettuce)" stopOpacity={0} />
+          </linearGradient>
+        </defs>
 
-        <Area type="monotone" dataKey="Water" stroke="var(--lettuce-secondary)" fill="url(#colorPv)" activeDot={{ r: 9 , fill: "white", stroke:"var(--lettuce-secondary)"}}/>
-      <Tooltip separator=" : " animationEasing="ease-in-out" animationDuration="300ms"
-      formatter={(value, name, props) => {
-        props.color = "var(--dark)"
-          return `${value} ml`;
-      }} />
-      <YAxis dataKey="Water" unit="L"  tick={{stroke: 'black', strokeWidth: 0.2}} axisLine={false} tickLine={false} />
-      <XAxis dataKey="date" tick={{stroke: 'var(--dark)', strokeWidth: 0.2}} axisLine={false} tickLine={false} padding={{ left: paddingGraph, right: 10 }}/>
-    </AreaChart>
+        <Area
+          type="monotone"
+          dataKey="Water"
+          stroke="var(--lettuce-secondary)"
+          fill="url(#colorPv)"
+          activeDot={{
+            r: 9,
+            fill: 'white',
+            stroke: 'var(--lettuce-secondary)',
+          }}
+        />
+        <Tooltip
+          separator=" : "
+          animationEasing="ease-in-out"
+          animationDuration="300ms"
+          formatter={(value, name, props) => {
+            props.color = 'var(--dark)';
+            return `${value} ml`;
+          }}
+        />
+        <YAxis
+          dataKey="Water"
+          unit="L"
+          tick={{
+            stroke: 'black',
+            strokeWidth: 0.2,
+            style: { transform: 'translateX(-11px)' },
+          }}
+          axisLine={false}
+          tickLine={false}
+        />
+        <XAxis
+          dataKey="date"
+          tick={{
+            stroke: 'var(--dark)',
+            strokeWidth: 0.2,
+            style: { transform: 'translateY(15px)' },
+          }}
+          axisLine={false}
+          tickLine={false}
+          padding={{ left: paddingGraph, right: 10 }}
+        />
+      </AreaChart>
+    </div>
   );
 };
