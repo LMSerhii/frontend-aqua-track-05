645459dde84ee26dd002119faedd1604d5d41e78
KaterynaMishchuk12 <138882300+KaterynaMishchuk12@users.noreply.github.com>
2024-05-18 15:49:51 +0300

    fixed percentage in calendar

--- src/components/trackerPage/Calendar/Calendar.jsx
+++ src/components/trackerPage/Calendar/Calendar.jsx
@@ -2,15 +2,35 @@ import { CalendarItem } from '../CalendarItem/CalendarItem';
 import { useAuth } from '../../../hooks/useAuth';
 import { useSelector } from 'react-redux';
 import { useGetAllEntriesByMonthQuery } from '../../../redux/tracker/trackerApi';
-import { setMonth } from '../../../redux/date/dateSlice';
+import { selectMonth } from '../../../redux/date/dateSlice';
 
 import s from './Calendar.module.css';
 
 export const Calendar = ({ selectedDate }) => {
-  const month = useSelector(setMonth);
+  const month = useSelector(selectMonth);
   const { user } = useAuth();
-  // const { data, isLoading, isError } = useGetAllEntriesByMonthQuery(month);
-  // console.log(data);
+  const { data, isLoading, isError } = useGetAllEntriesByMonthQuery(month);
+  console.log(data);
+  console.log(data.data);
+  const DataArray = data.data;
+  console.log(DataArray);
+
+  const byOneDayRecords = DataArray.map(({ date, amounts }) => {
+    const dateOfRecord = date;
+    const allAmountsPerDay = amounts;
+    return { date: dateOfRecord, amounts: allAmountsPerDay };
+  });
+  console.log('всі записи поденно', byOneDayRecords);
+
+  const dateAndTotalWater = byOneDayRecords.map(oneDay => {
+    const date = oneDay.date;
+    const totalWaterByDay = oneDay.amounts.reduce(
+      (acc, curr) => acc + curr.amount,
+      0
+    );
+    return { date: date, totalWaterByDay: totalWaterByDay };
+  });
+  console.log(dateAndTotalWater);
 
   const daysInMonth = new Date(
     selectedDate.getFullYear(),
@@ -22,20 +42,33 @@ export const Calendar = ({ selectedDate }) => {
     { length: daysInMonth },
     (_, index) => index + 1
   );
+  // console.log(user.dailyWater);
   // тут потрібен ще dailyWater з документа Юзера
-  // const ArrayOfWaterInPercent = data.map(item =>
-  //   Math.round((item.totalAmount * 100) / user.dailyWater)
+  // const inPercentArray = dateAndTotalWater.map(item =>
+  //   Math.round((item.totalWaterByDay * 100) / 2000)
   // );
+  // console.log(inPercentArray);
 
   return (
     <>
       <ul className={s.list}>
-        {daysArray.map(day => {
+        {daysArray.map((day, index) => {
+          const dayOfMonth = day < 10 ? `0${day}` : `${day}`;
+          const matchedItem = dateAndTotalWater.find(
+            item => item.date.substring(0, 2) === dayOfMonth
+          );
+          const matchedAmount = matchedItem
+            ? Math.round((matchedItem.totalWaterByDay * 100) / 2000)
+            : 0;
+
           return (
             <li key={day} className={s.item}>
-              <CalendarItem day={day} amount={100} />
+              <CalendarItem day={day} amount={index} />
+              {console.log(matchedAmount)}
 
-              <div className={s.percentage}></div>
+              <div className={s.percentage}>
+                {matchedAmount !== undefined ? `${matchedAmount}%` : '0%'}
+              </div>
             </li>
           );
         })}
