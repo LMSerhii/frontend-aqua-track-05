91d5468f3efa66d89c212cb04824c21be17ede57
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-05-18 23:53:10 +0300

    update

--- src/shared/helpers/formatDate.js
+++ src/shared/helpers/formatDate.js
@@ -3,6 +3,5 @@ import { format, parse } from 'date-fns';
 export const formatDate = date => {
   const parsedDate = parse(date, 'dd-MM-yyyy', new Date());
   const formattedDate = format(parsedDate, 'd, MMMM');
-  console.log('formattedDate', formattedDate);
   return { parsedDate, formattedDate };
 };
