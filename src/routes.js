7b8750727a9c2ad8138d6c9582f8a92dc24998d3
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-05-20 19:27:09 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -14,5 +14,5 @@ export const routes = {
   VERIFY: '/verify',
 };
 
-export const BASE_URL = 'http://localhost:3001/api/v1';
-// export const BASE_URL = 'https://backend-aqua-track-05.onrender.com/api/v1';
+// export const BASE_URL = 'http://localhost:3001/api/v1';
+export const BASE_URL = 'https://backend-aqua-track-05.onrender.com/api/v1';
