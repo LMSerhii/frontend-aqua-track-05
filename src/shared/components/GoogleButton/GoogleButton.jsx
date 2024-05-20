3a311abc8aef12eabd06df3d828ac88ca6e42074
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-05-20 18:52:51 +0300

    Update GoogleButton.jsx

--- src/shared/components/GoogleButton/GoogleButton.jsx
+++ src/shared/components/GoogleButton/GoogleButton.jsx
@@ -1,15 +1,13 @@
 import s from './GoogleButton.module.css';
 import SharedSVG from '../SharedSVG/SharedSVG';
 import { useTranslation } from 'react-i18next';
+import { BASE_URL } from '../../../routes';
 
 const GoogleButton = () => {
   const { t } = useTranslation();
   return (
     <>
-      <a
-        className={s.googleButton}
-        href="http://localhost:3001/api/v1/auth/google"
-      >
+      <a className={s.googleButton} href={`${BASE_URL}/auth/google`}>
         <SharedSVG svgId="google" width={32} height={32} />
 
         {t('singInForm.googleButton.googleBtn')}
