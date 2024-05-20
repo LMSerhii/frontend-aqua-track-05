a7ea335434713d64ec89219b40ec63b97c3ad06a
ImaLorikI <pasha.bandrivskiy@gmail.com>
2024-05-20 12:42:49 +0300

    fix left

--- src/components/homePage/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/homePage/AdvantagesSection/AdvantagesSection.jsx
@@ -37,49 +37,54 @@ export default function AdvantagesSection() {
         className={css.section}
       />
       <div className={css.div}>
-        <ul className={css.photo_list}>
-          <li className={css.list_items}>
-            <Picture
-              mob={male1}
-              tab={male1}
-              desc={male1}
-              mob2x={male2x}
-              tab2x={male2x}
-              desc2x={male2x}
-              width="47px"
-              height="47px"
-            />
-          </li>
-          <li className={css.list_items}>
-            <Picture
-              mob={male2}
-              tab={male2}
-              desc={male2}
-              mob2x={male22x}
-              tab2x={male22x}
-              desc2x={male22x}
-              width="47px"
-              height="47px"
-            />
-          </li>
-          <li className={css.list_items}>
-            <Picture
-              mob={male3}
-              tab={male3}
-              desc={male3}
-              mob2x={male32x}
-              tab2x={male32x}
-              desc2x={male32x}
-              width="47px"
-              height="47px"
-            />
-          </li>
-        </ul>
-        <p className={css.parag}>
-          {t('advantagesSection.ourParag')}{' '}
-          <span className={css.span}>{t('advantagesSection.happySpan')}</span>{' '}
-          {t('advantagesSection.customersParag')}
-        </p>
+        <div className={css.list_div}>
+          <ul className={css.photo_list}>
+            <li className={css.list_items}>
+              <Picture
+                mob={male1}
+                tab={male1}
+                desc={male1}
+                mob2x={male2x}
+                tab2x={male2x}
+                desc2x={male2x}
+                width="47px"
+                height="47px"
+              />
+            </li>
+            <li className={css.list_items}>
+              <Picture
+                mob={male2}
+                tab={male2}
+                desc={male2}
+                mob2x={male22x}
+                tab2x={male22x}
+                desc2x={male22x}
+                width="47px"
+                height="47px"
+              />
+            </li>
+            <li className={css.list_items}>
+              <Picture
+                mob={male3}
+                tab={male3}
+                desc={male3}
+                mob2x={male32x}
+                tab2x={male32x}
+                desc2x={male32x}
+                width="47px"
+                height="47px"
+              />
+            </li>
+            <p className={css.parag}>
+              {t('advantagesSection.ourParag')}{' '}
+              <span className={css.span}>
+                {t('advantagesSection.happySpan')}
+              </span>{' '}
+              {t('advantagesSection.customersParag')}
+            </p>
+          </ul>
+        </div>
+
         <ul className={css.list_trk}>
           <li className={css.item_trk}>
             <div className={css.flex}>
